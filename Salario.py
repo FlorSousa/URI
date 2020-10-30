93a2a87e2eeeff358d138c66dd2f32be40a201c8
Jhonatas Flor de Sousa <58887452+FlorSousa@users.noreply.github.com>
2020-10-30 04:49:21 -0300

    1008

---
+++ Salario.py
@@ -0,0 +1,6 @@
+Numero = int(input())
+HorasTrabalhadas = int(input())
+ValorHora = float(input())
+SALARY = ValorHora*HorasTrabalhadas
+print("NUMBER = %d" %Numero)
+print("SALARY = U$ %.2f" %SALARY)
